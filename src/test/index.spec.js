6b682cb493c4ca06e9b71629d438e0e6afcff81b
Paul Werner <paulwerner@protonmail.com>
2018-07-02 00:57:56 +0200

    reformat test

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,26 +1,28 @@
-const supertest = require('supertest');
+const supertest = require('supertest')
 
-describe('Test index returns json', function () {
-    let server;
-    let request;
+describe('Test index returns json', () => {
+    let server
+    let request
 
-    before(function () {
-        server = require('../main/main');
-        request = supertest.agent(server);
-    });
-    after(function (done) {
-        server.close(done);
-    });
+    before(() => {
+        server = require('../main/main')
+        request = supertest.agent(server)
+      }
+    )
+    after((done) => {
+      server.close(done)
+    })
 
-    it('should respond with 200 and Hello World', function (done) {
-        request
-            .get('/api')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, {"message": "Hello World!"})
-            .end(function (err) {
-                if (err) done(err);
-                done();
-            });
-    });
-});
+    it('should respond with 200 and Hello World', (done) => {
+      request
+        .get('/api')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {'message': 'Hello World!'})
+        .end((err) => {
+          if (err) done(err)
+          done()
+        })
+    })
+  }
+)
