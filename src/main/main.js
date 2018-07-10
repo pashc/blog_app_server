5c119566ed0cf6158d70cf01db41d597b329311d
Paul Werner <paulwerner@protonmail.com>
2018-07-10 14:43:33 +0200

    add mongodb connection with mongoose

--- src/main/main.js
+++ src/main/main.js
@@ -1,25 +1,48 @@
-import express from 'express'
 import bodyParser from 'body-parser'
+import express from 'express'
+import mongoose from 'mongoose'
 
-const port = process.env.SERVER_PORT || 3000;
+// =================================================
+//                      Config
+// =================================================
 
-const app = express();
+const port = process.env.SERVER_PORT || 3000,
+  db_host = process.env.DB_HOST || 'localhost',
+  db_port = process.env.DB_PORT || 27017,
+  database = process.env.DATABASE || 'blog'
 
-// SETUP
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// =================================================
+//                      Setup
+// =================================================
 
-// ROUTES
-const router = express.Router();
+// express
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// database
+const db_url = `mongodb://${db_host}:${db_port}/${database}`
+mongoose.connect(db_url)
+  .then(() => {
+    console.log('successfully connected to ' + db_url)
+  })
+
+// =================================================
+//                      Routes
+// =================================================
+
+const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.json({message: 'Hello World!'})
-});
+  res.json({message: 'Hello World!'})
+})
 
-// REGISTER ROUTES
-app.use('/api', router);
+app.use('/api', router)
 
-// START SERVER ON PORT
-const server = app.listen(port);
+// =================================================
+//                      Start Server
+// =================================================
 
-module.exports = server;
+const server = app.listen(port)
+
+module.exports = server
