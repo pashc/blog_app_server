7df6e77ad07aa2cd424d5816403fd1a917f1d678
Paul Werner <paulwerner@protonmail.com>
2018-07-22 19:10:54 +0200

    add request logging

--- src/main/main.js
+++ src/main/main.js
@@ -1,12 +1,16 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
 import mongoose from 'mongoose'
+import morgan from 'morgan'
 
 // =================================================
 //                      Config
 // =================================================
 
-const port = process.env.SERVER_PORT || 3000,
+const node_env = process.env.NODE_ENV || 'dev',
+  port = process.env.SERVER_PORT || 3000,
   db_host = process.env.DB_HOST || 'localhost',
   db_port = process.env.DB_PORT || 27017,
   database = process.env.DATABASE || 'blog'
@@ -26,6 +30,24 @@ mongoose.connect(db_url)
   .then(() => {
     console.log('successfully connected to ' + db_url)
   })
+
+// request logging
+let flag,
+  log_dir
+
+if ('dev' !== node_env) {
+  log_dir = path.join(process.cwd(), 'log/access.log')
+  flag = 'a'
+  app.use(morgan('combined'))
+
+} else {
+  log_dir = path.join(process.cwd(), 'log/dev/access.log')
+  flag = 'w'
+  app.use(morgan('dev'))
+}
+
+app.use(morgan('common', {stream: fs.createWriteStream(log_dir, {flags: flag})}))
+
 
 // =================================================
 //                      Routes
